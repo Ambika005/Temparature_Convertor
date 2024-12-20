41bf901c5534346ca6520b5a6ef918a77efabd1a
Ambika005 <ambikaselvaraj22@gmail.com>
2024-12-21 01:59:44 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,13 @@
+const celsius = document.getElementById("celsius");
+const fahrenheit = document.getElementById("fahrenheit");
+
+function CelToFar(){
+    let output = (parseFloat(celsius.value)*(9/5)+32);
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+function FarToCel(){
+    let output =(parseFloat(fahrenheit.value)-32*5/9);
+    celsius.value = parseFloat(output.toFixed(2));
+    console.log(output)
+}
